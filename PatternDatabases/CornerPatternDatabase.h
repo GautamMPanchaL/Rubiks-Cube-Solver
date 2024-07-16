89545be8521ebf3cafab951cea83896fa81fcf41
unknown <gautampanchal99204@gmail.com>
2024-07-16 21:47:20 +0530

    database creation

---
+++ PatternDatabases/CornerPatternDatabase.h
@@ -0,0 +1,28 @@
+//
+// Created by gauta on 15-07-2024.
+//
+
+#ifndef CORNERPATTERNDATABASE_H
+#define CORNERPATTERNDATABASE_H
+
+#include "../Model/RubiksCube.h"
+#include "PatternDatabase.h"
+#include "PatternDatabases.h"
+#include "PermutationIndexer.h"
+using namespace std;
+
+class CornerPatternDatabase : public PatternDatabase {
+
+    typedef RubiksCube::FACE F;
+
+    PermutationIndexer<8> permIndexer;
+
+public:
+    CornerPatternDatabase();
+    CornerPatternDatabase(uint8_t init_val);
+    uint32_t getDatabaseIndex(const RubiksCube& cube) const;
+
+};
+
+
+#endif //CORNERPATTERNDATABASE_H
